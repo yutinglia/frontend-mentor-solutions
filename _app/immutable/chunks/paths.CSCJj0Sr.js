124c734b2c19ca9c8709d3558e9ac66e2977333d
yutinglia <yutinglia@gmail.com>
2024-04-20 07:31:52 +0800

    Updates

--- _app/immutable/chunks/paths.CSCJj0Sr.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1p35oxc)==null?void 0:s.base)??"/frontend-mentor-solutions";var t;const o=((t=globalThis.__sveltekit_1p35oxc)==null?void 0:t.assets)??e;export{o as a,e as b};
