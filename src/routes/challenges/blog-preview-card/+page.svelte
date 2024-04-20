e9fee4788356809ce525daaa2968cc55f11e2f5b
yutinglia <yutinglia@gmail.com>
2024-04-21 07:37:05 +0800

    Complete Blog Preview Card

--- src/routes/challenges/blog-preview-card/+page.svelte
+++ src/routes/challenges/blog-preview-card/+page.svelte
@@ -1,3 +1,23 @@
+<script lang="ts">
+	import Card from './Card.svelte';
+</script>
+
+{#snippet header()}
+	<h1>Test</h1>
+{/snippet}
+
+<main class="flex h-dvh bg-[color:hsl(47,88%,63%)]">
+	<Card
+		img="/blog-preview-card/illustration-article.svg"
+		tag="Learning"
+		date={new Date(2023, 11, 21)}
+		title="HTML & CSS foundations"
+		description="These languages are the backbone of every website, defining structure, content, and presentation."
+		author="Greg Hooper"
+		avatar="/blog-preview-card/image-avatar.webp"
+	/>
+</main>
+
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&family=Outfit:wght@100..900&display=swap');
 </style>
---
+++ src/routes/challenges/blog-preview-card/Card.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { getStaticPath } from '$lib/path';
+	import { format } from 'date-fns';
+
+	type Props = {
+		img: string;
+		tag: string;
+		date: Date;
+		title: string;
+		description: string;
+		author: string;
+		avatar: string;
+	};
+
+	let { ...props }: Props = $props();
+</script>
+
+<div
+	class="group/card font-[Figtree] border border-black m-auto flex flex-col justify-between shadow-[0.5rem_0.5rem_0px_0px_rgba(0,0,0,1)] p-[18px] rounded-[20px] w-[327px] h-[501px] bg-[color:hsl(0,0%,100%)] hover:-translate-x-[0.25rem] hover:-translate-y-[0.25rem] hover:shadow-[0.75rem_0.75rem_0px_0px_rgba(0,0,0,1)] hover:cursor-pointer transition"
+>
+	<div>
+		<img class="rounded-[10px]" src={getStaticPath(props.img)} alt="Blog Preview" />
+	</div>
+	<div class="flex flex-col">
+		<div class="mb-3">
+			<span class="bg-[color:hsl(47,88%,63%)] text-[14px] px-1.5 py-1 font-bold rounded-sm">
+				{props.tag}
+			</span>
+		</div>
+		<p class="text-[color:hsl(0,0%,7%)] text-[12px] mb-3">
+			Published {format(props.date, 'dd MMM yyyy')}
+		</p>
+		<h2
+			class="text-[color:hsl(0,0%,7%)] font-bold text-[20px] font-[Figtree] mb-3 group-hover/card:text-[color:hsl(47,88%,63%)] transition"
+		>
+			{props.title}
+		</h2>
+		<p class="text-[color:hsl(0,0%,50%)] text-[14px]">{props.description}</p>
+	</div>
+	<div>
+		<div class="flex items-center mb-2">
+			<img
+				src={getStaticPath(props.avatar)}
+				alt="Author Avatar"
+				class="w-[30px] h-[30px] rounded-full"
+			/>
+			<div class="flex flex-col ml-2">
+				<span class="text-[color:hsl(0,0%,10%)] text-[14px] font-bold">{props.author}</span>
+			</div>
+		</div>
+	</div>
+</div>
