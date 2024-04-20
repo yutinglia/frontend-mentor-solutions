b00962ccca24042c7a3534791d6153434ffd1d4e
yutinglia <yutinglia@gmail.com>
2024-04-20 21:49:02 +0800

    new static folder

--- src/routes/challenges/qr-code-component/+page.svelte
+++ src/routes/challenges/qr-code-component/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="p-20 bg-[color:hsl(212,45%,89%)] h-dvh flex">
 	<Card
-		img="/image-qr-code.png"
+		img="/qr-code-component/image-qr-code.png"
 		header="Improve your front-end skills by building projects"
 		content="Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
 	/>
